088a1b02931751f47495d34a4447948111c18360
Adejumo Adedamola <damoladavidadejumo@gmail.com>
2024-12-15 22:42:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let addToCartButtons = document.getElementsByClassName('btn-primary')
 let cartContainer = document.getElementsByTagName('tbody')[0]
 let quantityFields = document.getElementsByClassName('num')
 let delete_buttons = document.getElementsByClassName('uk-button-danger')
-let cartCounter 
+let cartCounter = document
 
 
 for(let i = 0; i < addToCartButtons.length; i++){
